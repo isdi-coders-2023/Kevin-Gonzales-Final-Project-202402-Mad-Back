124377e219e52ba84e079a68a37f289bfde101dd
Kev <kev@MacBook-Air-de-Kevin.local>
2024-05-19 20:19:04 +0200

    sonar issues fixed

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -61,14 +61,14 @@ export class UsersService {
   }
 
   async findOneUser(id: string): Promise<User> {
-    const data = this.prismaService.user.findUnique({
-      where: { id },
-      select: selectFull,
-    });
-    if (!data) {
+    try {
+      return this.prismaService.user.findUnique({
+        where: { id },
+        select: selectFull,
+      });
+    } catch (error) {
       throw new NotFoundException(`User ${id} not founded`);
     }
-    return data;
   }
 
   async filterUsers(data: FilterUserDto): Promise<User[]> {
@@ -78,7 +78,7 @@ export class UsersService {
     });
   }
 
-  async createUser(data: CreateUserDto) {
+  async createUser(data: CreateUserDto): Promise<User> {
     try {
       return this.prismaService.user.create({ data }) as unknown as User;
     } catch (error) {
